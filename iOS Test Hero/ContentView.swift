7db4bc11c60c97fc447dd8bb112c77d3d0900606
AddinDev <addinsatria2004@gmail.com>
2022-03-08 21:24:41 +0700

    Initial Commit

---
+++ iOS Test Hero/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Test Hero
+//
+//  Created by Addin Satria on 08/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Test Hero/iOS_Test_HeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Test_HeroApp.swift
+//  iOS Test Hero
+//
+//  Created by Addin Satria on 08/03/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Test_HeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
